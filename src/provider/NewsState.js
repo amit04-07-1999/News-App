33dc4b1e6b8b8f003fc818003f4677b2351fe737
amit04-07-1999 <ameykumar76@gmail.com>
2024-04-01 16:57:28 +0530

    s

--- src/provider/NewsState.js
+++ src/provider/NewsState.js
@@ -4,7 +4,8 @@ import NewsProvider from "./NewsProvider";
 function NewsState({ children }) {
   const [newsapi, setNewsApi] = useState([]);
   const fetchData = async (title) => {
-    const res = await fetch(`https://newsapi.org/v2/everything?q=${title}&apiKey=96d96aaf3a7d4063b8ae013fc0610f80`);
+    // const res = await fetch(`https://newsapi.org/v2/everything?q=${title}&apiKey=96d96aaf3a7d4063b8ae013fc0610f80`);
+    const res = await fetch(`https://gnews.io/api/v4/top-headlines?q=${title}&apikey=1b433f2a2e9b6997f0259270cd0b3af8`);
     const resData = await res.json();
     setNewsApi(resData.articles);
   };
